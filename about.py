775d686c6316536c3701036ad2acc842a1f12d9e
anonboy96 <118814879+anonboy96@users.noreply.github.com>
2023-08-01 20:09:44 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,33 @@
+def about():
+    print('''
+    
+    All Bombs away Sir
+      \
+           \      Goodbye Dullsville!
+                __|__  /
+       .'(\      .-.     /)'.
+    +-====(*)===: " :===(o)=====-+
+            \).  '-'  .(/                            
+                 +=
+                 +=
+                 +=           █▄▄ █▀█ █▀▄▀█ █▄▄ █▀▀ █▀█ ▀█▀ █ █ █▀█ █▄ █
+                              █▄█ █▄█ █ ▀ █ █▄█ ██▄ █▀▄  █  █▀█ █▄█ █ ▀█
+                 += 
+                 +=          ##############################################
+                 +=          # Version >>> v1.0 (stable)                  #
+                             # Last Update >>> 1st Aug 2023               #
+                 +=          # Coded by Umang (Anonboy96)                 #
+                 +=          # GitHub >>> https://github.com/anonboy96    #
+                 +=          ##############################################
+''')
+
+def menu():
+     print('''
+    |-PRESS---------------|
+    | 1] Call Bomber      |
+    | 2] SMS Bomber       |
+    | 3] Instagram Bomber |
+    | 4] WhatsApp Bomber  |
+    | 5] About            |
+    | 6] Exit Script      |
+    |---------------------|''')
